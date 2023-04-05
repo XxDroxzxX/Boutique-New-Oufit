9a23fcccbe1be6f29e8718e56c8ed64d55a94251
manuel carrascal <tricdiart@gmail.com>
2023-04-04 23:32:53 -0500

    se añadieron los comentarios correspondientes en el codigo del archivo togglePassword.js para saber que es lo que realiza el codigo

--- views/js/togglePassword.js
+++ views/js/togglePassword.js
@@ -1,7 +1,14 @@
 "use strict";
 
+//se declaran las constantes que almacenaran las propiedades de los id's correspondientes.
 const togglePassword =document.querySelector("#togglePassword");
 const password = document.querySelector("#inputPassword");
+
+/*
+Por medio de un addEventListener sabemos si el usuario pulso el icono del candado y si esto es asi se captura
+el tipo actual con el que cuenta el campo haciendo posible que por medio de un operador ternario cambiemos al otro estado que queremos
+y de la misma forma con operador termario cambiamos la apariencia del icono dependiendo del atributo type que tengo el input.
+*/
 
 togglePassword.addEventListener("click",()=>{
     const type = password.getAttribute("type")==="password"? "text":"password";
